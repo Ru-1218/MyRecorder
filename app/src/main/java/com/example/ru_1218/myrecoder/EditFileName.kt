911bb7e84a5dff89981392c09f0565351f736dc7
Ryoji Uehara <orcinusorca1758dv6932@gmail.com>
2020-11-22 11:18:20 +0900

    ファイル名の変更->パスの書き換えはクリア

--- app/src/main/java/com/example/ru_1218/myrecoder/EditFileName.kt
+++ app/src/main/java/com/example/ru_1218/myrecoder/EditFileName.kt
@@ -4,45 +4,61 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import java.io.File
+import kotlin.IllegalStateException
+import android.util.Log
 
 
-class EditFileName(playback_path: String?, uuid: String): DialogFragment() {
-    private var path:String? = playback_path
+class EditFileName(playback_path: String?, uuid: String): DialogFragment(){
+    private var path:String = playback_path as String
     private var uuid:String  = uuid
-    val inflater = requireActivity().layoutInflater;
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        //ダイアログビルダを生成
-        val builder = AlertDialog.Builder(activity)
-        //ダイアログのタイトルを設定
-        builder.setTitle("Edit File Name")
-        //ビューの読み込み
 
-        builder.setView(inflater.inflate(R.layout.edit_filename, null))
-        builder.setMessage(uuid)
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater
 
-        //ボタンイベント
-        builder.setPositiveButton(R.string.MenuListEdit, DialogButtonClickListener())
+            builder.setView(inflater.inflate(R.layout.edit_filename, null))
+                .setMessage(uuid)
+                .setTitle("ファイル名を変更する")
+                .setPositiveButton("変更", DialogInterface.OnClickListener { dialog, id ->
+                    var edit = getDialog()?.findViewById<EditText>(R.id.edit_filename)
+                    var name = edit?.text.toString() //入力値を受け取り
 
-        return builder.create()
+
+
+                    var slicePath: List<String> = path.split("/")
+                    Log.e("test", slicePath.toString());
+                    var getParentDir= slicePath.dropLast(1)
+                    Log.e("test", getParentDir.toString());
+                    var dir: String = getParentDir.joinToString("/")
+                    Toast.makeText(activity, "名前を変更しました", Toast.LENGTH_LONG).show()
+//
+//
+//
+                    File(dir, "$uuid.3gp")
+                        .copyTo(File(dir, "$name.3gp"), true)
+
+//                    元のファイルの削除
+                    File(dir, "$uuid.3gp").delete()
+
+
+                })
+                .setNegativeButton("閉じる",DialogInterface.OnClickListener{ dialog, id ->
+                    getDialog()?.cancel()
+//                    リロード処理
+
+                })
+            builder.create()
+        }?: throw IllegalStateException("Activrt Cannot be null")
+
     }
 
-    private inner class DialogButtonClickListener: DialogInterface.OnClickListener {
-        override fun onClick(dialog: DialogInterface?, which: Int) {
 
 
-            when(which){
-
-                DialogInterface.BUTTON_POSITIVE->{
-                    //ファイル名の変更もといパス情報も書き換える
-
-
-                }
-
-            }
-
-        }
-
-    }
 }
+
--- app/src/main/java/com/example/ru_1218/myrecoder/EditFileNameActivity.kt
+++ app/src/main/java/com/example/ru_1218/myrecoder/EditFileNameActivity.kt
@@ -2,10 +2,82 @@ package com.example.ru_1218.myrecoder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.SimpleAdapter
 
 class EditFileNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_file_name)
+
+        val fileNmae = intent.getSerializableExtra("EditNames") as ArrayList<*>
+
+        val fileList = findViewById<ListView>(R.id.voiceListMenu)
+        fileList.onItemClickListener = ListItemClickListener(fileNmae)
+
+
+        val voiceList: MutableList<MutableMap<String, String>> = mutableListOf()
+
+        fileNmae.forEach {
+            path ->
+            var path = path as String
+            var voiceMenu = getFileName(path)
+            voiceList.add(voiceMenu)
+
+        }
+
+        val from = arrayOf("name")
+        val to = intArrayOf(R.id.EditFileName_row)
+
+        val adapter = SimpleAdapter(applicationContext, voiceList, R.layout.activity_edit_row, from, to)
+        fileList.adapter = adapter
     }
+
+    private fun getFileName(path: String): MutableMap<String, String> {
+        var file: List<String> = path.split("/")
+        var fileName = file.takeLast(1).first()
+        var uuid: String = fileName.replace(".3gp", "")
+        var voiceMenu = mutableMapOf("name" to uuid)
+        return voiceMenu
+    }
+
+    private inner class ListItemClickListener(dataList: ArrayList<*>) : AdapterView.OnItemClickListener {
+        private var data = dataList
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            val item= parent?.getItemAtPosition(position) as MutableMap<String, String>
+
+            val uuid = item["name"] as String
+            val dataPath = sameNameFilePath(data, uuid)
+            val dialogFragment = EditFileName(dataPath, uuid)
+            dialogFragment.show(supportFragmentManager, "DialogEvent")
+
+        }
+
+    }
+
+    private fun checkFileName(path: String): String{
+        var file: List<String> = path.split("/")
+        var fileName = file.takeLast(1).first()
+        var uuid: String = fileName.replace(".3gp", "")
+        return uuid
+    }
+
+    private fun sameNameFilePath(datas: ArrayList<*>, fileName: String): String? {
+        datas.forEach { path ->
+            print(path)
+            var path = path
+            var voiceMenu: String = checkFileName(path as String)
+            if (voiceMenu == fileName) {
+                return path
+            }
+
+        }
+        return "test"
+    }
+
+
+
+
 }
